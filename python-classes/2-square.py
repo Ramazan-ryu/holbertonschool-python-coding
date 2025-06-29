cf52e43a677052b1c557ded183a2f0c3132a073b
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-29 13:16:33 +0000

    2-square.py--2-main.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,14 +4,14 @@
 
 class Square:
     """A class that initializes an object with a private attribute size"""
-    
+
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         return self.__size * self.__size
-
