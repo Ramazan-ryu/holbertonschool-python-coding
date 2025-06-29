148f9085cad10ab22233fe53bab114343c1ac6c0
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-29 12:58:55 +0000

    1-square.py-1main.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """A class that initializes an object with a private attribute size"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
