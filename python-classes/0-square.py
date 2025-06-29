31297fc69650eca999ead72df004671685e08744
ramazan_mustafayev <mustafayevramazan121@gmail.com>
2025-06-29 12:34:02 +0000

    0-square.py/0-main.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""a class Square that defines a square by: (based on 0-square.py)"""
+
+
 class Square:
-    """This module defines a Square class."""
+    """A class that initializes an object with a private attribute size"""
+
     def __init__(self, size):
         self.__size = size
